c9f5265c7c6b498c6f6e86e3e3e28e2c2f54c63e
Abraao-Junior <104397859+Abraao-Junior@users.noreply.github.com>
2023-11-21 03:01:19 -0300

    Crud da Tela de Fornecedor

--- cadastro_fornecedores/forms.py
+++ cadastro_fornecedores/forms.py
@@ -1,13 +1,13 @@
 from django import forms
 from .models import Fornecedor
 
-class FormCadastroFornecedor(forms.ModelForm):
+class FormFornecedor(forms.ModelForm):
     class Meta:
         model = Fornecedor
         fields = ('nome', 'documento', 'email', 'endereco', 'telefone')
         
     def __init__(self, *args, **kwargs):
-        super(FormCadastroFornecedor, self).__init__(*args, **kwargs)
+        super(FormFornecedor, self).__init__(*args, **kwargs)  # Corrigido o nome da classe aqui
         self.fields['nome'].widget.attrs.update({'class': 'form-control'})
         self.fields['documento'].widget.attrs.update({'class': 'form-control'})
         self.fields['email'].widget.attrs.update({'class': 'form-control'})
